afbcd2d99333a7cca40d7f8d74333575f7bc4cb5
unknown <s20153044@stdlan.hcs.local>
2017-12-06 16:25:30 +0900

    認証らへん devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/show'
-  root 'asoberu/asotop'
+  devise_for :users
+  root 'pages#show'
+  get 'pages/asotop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
