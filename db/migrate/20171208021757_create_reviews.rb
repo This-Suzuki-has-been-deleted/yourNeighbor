c8c2d8ff4ac7026a5568054f3e2f88ef94ff59e4
unknown <s20153044@stdlan.hcs.local>
2017-12-08 14:37:09 +0900

    いつだって本番環境

--- db/migrate/20171208021757_create_reviews.rb
+++ db/migrate/20171208021757_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
-    create_table :reviews, id: false do |t|
+    create_table :reviews, primary_key: 'reviews_id' 'email' 'maps_id', id: false do |t|
       t.string :reviews_id                  # レビューID
       t.string :review_title	              # レビュータイトル
     	t.string :review_text               	# レビュー本文
@@ -10,7 +10,7 @@ class CreateReviews < ActiveRecord::Migration[5.1]
       t.string :email                       # 外部キー：Users
       t.string :maps_id                     # 外部キー：Maps
       #主キー指定
-      PRIMARY KEY('reviews_id', 'email', 'maps_id')
+      PRIMARY KEY()
     end
   end
 end
