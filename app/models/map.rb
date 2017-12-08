1fc99f3ab6f85a3d7e3ddb86a8a04b5936140624
unknown <s20153044@stdlan.hcs.local>
2017-12-08 10:40:50 +0900

    まいぐれーしょんやりなおし

--- app/models/map.rb
+++
@@ -1,4 +0,0 @@
-class Map < ApplicationRecord
-  #self.primary_key = :point_id
-  #belongs_to :users,foreign_key: :email_id
-end
--- app/models/review.rb
+++
@@ -1,5 +0,0 @@
-class Review < ApplicationRecord
-  #self.primary_key = :review_id
-  #belongs_to :maps, foreign_key: :point_id
-  #belongs_to :users, foreign_key: :email_id
-end
