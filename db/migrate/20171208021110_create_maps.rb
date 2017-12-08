421491fedf96fd56009fc49599eb531f6515a2b2
unknown <s20153044@stdlan.hcs.local>
2017-12-08 11:29:05 +0900

    カラム追加

--- db/migrate/20171208021110_create_maps.rb
+++ db/migrate/20171208021110_create_maps.rb
@@ -1,10 +1,11 @@
 class CreateMaps < ActiveRecord::Migration[5.1]
   def change
-    create_table :maps, id=true do |t|
-      t.string :map_name  #場所名
-      t.string :map_lat   #緯度
-      t.string :map_lng   #経度
-      t.date :map_date    #追加日付
+    create_table :maps, primary_key: "maps_id" do |t|
+      t.string :maps_id               # ID
+      t.string :map_name              # 場所名
+      t.string :map_lat               # 緯度
+      t.string :map_lng               # 経度
+      t.date :map_date                # 追加日付
       t.timestamps
     end
   end
--- db/migrate/20171208021757_create_reviews.rb
+++ db/migrate/20171208021757_create_reviews.rb
@@ -1,10 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
-    create_table :reviews, id=true do |t|
-      t.string :review_title	#レビュータイトル
-    	t.string :review_text	#レビュー本文
-    	t.string :review_eva	#評価
-    	t.date :review_date	#追加日付
+    create_table :reviews, primary_key: "reviews_id" do |t|
+      t.string :reviews_id                  # レビューID
+      t.string :review_title	              # レビュータイトル
+    	t.string :review_text               	# レビュー本文
+    	t.string :review_eva, default: "good"	# 評価
+    	t.date :review_date                  	# 追加日付
       t.timestamps
     end
   end
