24595f6dc79f7261ec7df1d2609adbc6c5adbbba
unknown <s20153044@stdlan.hcs.local>
2017-12-08 14:11:04 +0900

    a

--- db/migrate/20171208021757_create_reviews.rb
+++ db/migrate/20171208021757_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
-    create_table :reviews, id= false, primary_key: "reviews_id" do |t|
+    create_table :reviews, id: false, primary_key: "reviews_id" do |t|
       t.string :reviews_id                  # レビューID
       t.string :review_title	              # レビュータイトル
     	t.string :review_text               	# レビュー本文
