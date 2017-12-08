98a95ce17309c23a9acbbd970ca5b02e4d6045b1
unknown <s20153044@stdlan.hcs.local>
2017-12-08 13:59:54 +0900

    ああああああ

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,4 @@
 class Map < ApplicationRecord
-  belongs_to :users, foreign_key: "email"
+  belongs_to :users,primary_key: "email," foreign_key: "email"
   has_many :revews, foreign_key: "maps_id"
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :maps, foreign_key: "maps_id"
-  belongs_to :users. foreign_key: "email"
+  belongs_to :maps, primary_key: "maps_id", foreign_key: "maps_id"
+  belongs_to :users, primary_key: "email", foreign_key: "email"
 end
