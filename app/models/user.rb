9551e567ef579deab4677e28fcd4759edddac583
usk <hotmail08ts@gmail.com>
2017-12-08 09:33:16 +0900

    model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         database_authenticatable,
+         :length
 end
