4f7aac943f2d3327b51d30ed272f2de79cb5fc04
usk <hotmail08ts@gmail.com>
2017-12-08 10:00:45 +0900

    日本語変換yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,5 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #ここで接続先を変える？
 end
