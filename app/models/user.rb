b85044a30790190d91de596b35ef320ac9bf26e2
usk <hotmail08ts@gmail.com>
2017-12-08 09:38:47 +0900

    asda

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  self.primary_keys = :email_id
+  self.primary_keys = :"email_id"
   has_many :reviews, foreign_key: :email_id
   has_many :maps, foreign_key: :email_id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         database_authenticatable,
-         :length
 end
