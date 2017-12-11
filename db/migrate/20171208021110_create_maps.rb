4576e5a651c033b3f68f854f62c8fa14a43a16ef
unknown <s20153044@stdlan.hcs.local>
2017-12-11 10:15:19 +0900

    aliyfr;iufroliufr

--- db/migrate/20171208021110_create_maps.rb
+++ db/migrate/20171208021110_create_maps.rb
@@ -1,13 +1,13 @@
 class CreateMaps < ActiveRecord::Migration[5.1]
   def change
     create_table :maps,primary_key: 'maps_id' 'email', id: false do |t|
-      t.string :maps_id               # ID
-      t.string :map_name              # 場所名
-      t.string :map_lat               # 緯度
-      t.string :map_lng               # 経度
-      t.date :map_date                # 追加日付
+      t.string :maps_id                                  # ID
+      t.string :map_name                                # 場所名
+      t.string :map_lat                                 # 緯度
+      t.string :map_lng                                 # 経度
+      t.date :map_date                                  # 追加日付
       # ここから外部キー
-      t.string :email                 # 外部キー：Users
+      t.string :email                                    # 外部キー：Users
     end
   end
 end
