da71ebf0ebda18a8436195b33ab36bf958c6541a
usk <hotmail08ts@gmail.com>
2017-12-19 13:52:48 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#show'
   get 'pages/asotop'
-  #before_action:authenticate_user!
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
