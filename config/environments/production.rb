deb0a3033546443e7fab84a5fd510056f1f4c373
usk <hotmail08ts@gmail.com>
2017-12-20 14:33:22 +0900

    あ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,18 +89,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-# mailer setting
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-#ここで接続先を変える？
-# mail setting
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :user_name => "hotmail08ts@gmail.com",
-  :password => "yusuke081120",
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-end
