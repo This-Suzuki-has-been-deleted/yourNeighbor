f56e6c4c6589b9f6c7347b41937818c3f069fda4
usk <hotmail08ts@gmail.com>
2017-12-21 13:11:35 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :asoberu ,:as =>'asoberu', :only => [:new], :path => 'asoberu/map'
 
-  
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
