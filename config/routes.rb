1aeb5670eaae6c522f6cbcd19fa1f1722a898440
usk <hotmail08ts@gmail.com>
2017-12-21 12:15:38 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :asoberu , :only => [:new], :path => 'asoberu/map'
+
   root 'pages#index'
   get 'pages/show'
-
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
