8cff1778c3ed2a7ea384d086a591de69740ae986
unknown <s20153044@stdlan.hcs.local>
2017-12-21 14:03:52 +0900

    asdjk

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resource :'map/asotop'
+  resource :map do
+    get 'asotop', :as => :Top_Page
+  end
   resource :review
 end
