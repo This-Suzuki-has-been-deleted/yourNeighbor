5a138b0be8c3bfb192bb6fc553ee6f771720b7ce
usk <hotmail08ts@gmail.com>
2017-12-22 03:32:16 +0900

    s

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,9 +2,10 @@ class MapController < ApplicationController
   def asotop
     @places = Place.all
     @hash = Gmaps4rails.build_markers(@places) do |place, marker|
-   marker.lat place.latitude
-   marker.lng place.longitude
-   marker.infowindow place.name
+      marker.lat place.latitude
+      marker.lng place.longitude
+      marker.infowindow place.name
+    end
 
 
   #全ピンの情報を取得
