b199917f0ba2a6dbb1c0acb59083abccc0d21a37
usk <hotmail08ts@gmail.com>
2017-12-21 12:09:33 +0900

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "pages#index"
-
+  root 'pages#index'
+  get 'pages/show'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
