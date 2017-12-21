b43b58c6dce08f1a2b81d4dfe464d577ba813e22
usk <hotmail08ts@gmail.com>
2017-12-22 00:20:07 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
   # resources :maps, :only => [:new, :create, :edit, :update]
   #  get'maps/new' => 'maps/new#new'
 
   # resource :maps do
   #   get 'asotop', :as => :top_Page
   # end
+  asoberu_for :users
+  # resources :asoberu, :controller => :asoberu, :path =>'asoberu/maps', :only => [:new]
 
-  resources :asoberu, :controller => :asoberu, :path =>'asoberu/maps', :only => [:new]
-
-  root :controller => 'pages', :action => 'index'
+  root :controller => 'pages', :action => 'new'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
