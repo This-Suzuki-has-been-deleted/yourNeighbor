2d868ba834205926d0ef6743f4c04ff271ff0d66
usk <hotmail08ts@gmail.com>
2017-12-21 10:24:03 +0900

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
+
+  before_filter :set_current_user
+
+  def set_current_user
+    asoberu.current_user = current_user
+  end
+
 end
