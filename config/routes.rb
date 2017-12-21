8d6c6e121d461a0dc2b52b65c43a0c80117e552b
usk <hotmail08ts@gmail.com>
2017-12-22 00:50:59 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #   get 'asotop', :as => :top_Page
   # end
 
-   resources :asoberu, :controller => :map, :only => [:new]
+   resources :asoberu, :controller => :map, :path => 'asoberu/maps', :only => [:new]
 
   root :controller => 'pages', :action => 'index'
 
