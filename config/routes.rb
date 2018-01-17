f8fb04a32202ac80de3deaf91118cb099dee2d9a
unknown <s20153044@stdlan.hcs.local>
2018-01-17 13:51:22 +0900

    oh

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resource :maps
 
    # resources :asoberu, :controller => :map, :path => 'asoberu', :only => [:creates]
+  root :controller => 'pages', :action => 'index'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
