0d0054a5de0b8851abec5d1c2e2f39db61ab516b
unknown <s20153044@stdlan.hcs.local>
2018-01-17 14:53:12 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'maps#index'
+  get 'maps/show'
 
   devise_for :users
 
 
   # resources :maps, :only => [:new, :create, :edit, :update]
   #  get'maps/new' => 'maps/new#new'
-
-  resources :maps
   # root :controller => 'maps', :action => 'index'
 
   devise_scope :user do
