f877d3d4535d5e59f307c246f80aac6f8eabc8c6
unknown <s20153044@stdlan.hcs.local>
2018-01-17 15:02:26 +0900

    aaaa

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,14 +2,14 @@ class MapsController < ApplicationController
 
   #全ピンの情報を取得
   def index
-    @ping = map.all
+    @ping = maps.all
   end
 
   #検索ワードと一致するピンを表示
   def search
     if request.post? then
       word = params[:word]
-      @ping = map.where("map_name like '%" + word + "%'")
+      @ping = maps.where("map_name like '%" + word + "%'")
     end
   end
 
@@ -17,7 +17,7 @@ class MapsController < ApplicationController
   def new
     if request.post? then
       map_data = params[:map_data]
-      map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
+      maps.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
     end
   end
 
@@ -25,7 +25,7 @@ class MapsController < ApplicationController
   def edit
     if request.post? then
       map_id = params[:map_id]
-      map.update(maps_id: map_id)
+      maps.update(maps_id: map_id)
     end
   end
 
@@ -33,7 +33,7 @@ class MapsController < ApplicationController
   def delete
     if request.post? then
       map_id = params[:map_id]
-      map.destroy_all(maps_id: map_id)
+      maps.destroy_all(maps_id: map_id)
     end
   end
 
