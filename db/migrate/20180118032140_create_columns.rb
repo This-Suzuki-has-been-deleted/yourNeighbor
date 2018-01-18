161e20dc249ae77fb4bdbeb938178be33262d4fc
unknown <s20153044@stdlan.hcs.local>
2018-01-18 13:38:04 +0900

    aaaaa

--- db/migrate/20180118032140_create_columns.rb
+++ db/migrate/20180118032140_create_columns.rb
@@ -2,7 +2,7 @@ class CreateColumns < ActiveRecord::Migration[5.1]
   def change
     create_table :columns do |t|
       t.string :title
-      t.string :text
+      t.string :text, :limit => 1000
 
       t.timestamps
     end
