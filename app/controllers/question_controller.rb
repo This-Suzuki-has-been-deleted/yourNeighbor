270eff6a88f917099794afc836644c8e574d2bc3
unknown <s20153044@stdlan.hcs.local>
2018-01-18 12:20:53 +0900

    あ＾～

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,2 +1,23 @@
 class QuestionController < ApplicationController
+  # 一覧画面
+  def index
+    @question = Question.all
+  end
+
+  # 詳細画面
+  def show
+  end
+
+  # 新規作成画面
+  def new
+  end
+
+  # 新規作成処理 view無し
+  def create
+  end
+
+  # 削除処理
+  def destroy
+  end
+
 end
