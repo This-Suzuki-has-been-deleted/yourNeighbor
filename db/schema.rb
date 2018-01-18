5b51b613406b9ea1c8068f37e5927dbfc8da7dc0
root <root@ip-172-31-28-35.ap-northeast-1.compute.internal>
2018-01-18 11:32:01 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208021757) do
+ActiveRecord::Schema.define(version: 20180118022649) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "email"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "map_reports", force: :cascade do |t|
+    t.string "map_report_text"
+    t.string "maps_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "maps", id: false, force: :cascade do |t|
     t.string "maps_id"
@@ -21,6 +38,33 @@ ActiveRecord::Schema.define(version: 20171208021757) do
     t.string "email"
   end
 
+  create_table "prefs", force: :cascade do |t|
+    t.string "pref_name"
+    t.string "pref_lat"
+    t.string "pref_lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "quiestions", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.string "eva"
+    t.string "tag1"
+    t.string "tag2"
+    t.string "tag3"
+    t.string "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "review_reports", force: :cascade do |t|
+    t.string "review_report_text"
+    t.string "reviews_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reviews", id: false, force: :cascade do |t|
     t.string "reviews_id"
     t.string "review_title"
@@ -29,6 +73,12 @@ ActiveRecord::Schema.define(version: 20171208021757) do
     t.date "review_date"
     t.string "email"
     t.string "maps_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,6 +98,12 @@ ActiveRecord::Schema.define(version: 20171208021757) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.binary "userIcon"
+    t.string "userName"
+    t.string "userFullName"
+    t.string "userAddress"
+    t.string "userAge"
+    t.string "userType"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
