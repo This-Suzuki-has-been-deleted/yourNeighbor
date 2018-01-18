7ba23c595b08ac0a8b7b2f8d5bc82e47487ff982
unknown <s20153097@stdlan.hcs.local>
2018-01-18 14:24:53 +0900

    map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,6 +3,8 @@ class MapsController < ApplicationController
   #全ピンの情報を取得
   def index
     @ping = Map.all
+
+    end
   end
 
   #検索ワードと一致するピンを表示
@@ -13,12 +15,8 @@ class MapsController < ApplicationController
     end
   end
 
-  def new
-
-  end
-
   #登録
-  def create
+  def new
     if request.post? then
       map_data = params[:map_data]
       Map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
