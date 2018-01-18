4682b36023670fd25a554f2375269f30f7564e1a
root <root@ip-172-31-28-35.ap-northeast-1.compute.internal>
2018-01-18 13:35:15 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118022649) do
+ActiveRecord::Schema.define(version: 20180118032140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20180118022649) do
   create_table "answers", force: :cascade do |t|
     t.string "email"
     t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "columns", force: :cascade do |t|
+    t.string "title"
+    t.string "text", limit: 1000
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
