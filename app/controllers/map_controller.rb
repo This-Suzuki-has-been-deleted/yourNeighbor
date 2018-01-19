be8b7756b6eb419d5c9124abf6c9bb3fd7ea1df5
unknown <s20153097@stdlan.hcs.local>
2018-01-19 12:06:44 +0900

    m

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,13 +1,24 @@
 class MapController < ApplicationController
-
-  #全ピンの情報を取得
+  # GET /maps
+  # GET /maps.json
   def index
-    #@ping = map.all
-    @Maps = Map.all
-    @hash = Gmaps4rails.build_markers(@Maps) do |map, marker|
-      marker.lat map.latitude
-      marker.lng map.longitude
-    end
+    @maps = Map.all
+  end
+
+  # GET /maps/1
+  # GET /maps/1.json
+  def show
+    @map = Map.find_by(id: params[:id])
+    @latitude = @map.latitude
+
+    @longitude = @map.longitude
+
+    @address = @map.address
+  end
+
+  # GET /maps/new
+  def new
+    @map = Map.new
   end
 
   #検索ワードと一致するピンを表示
@@ -19,12 +30,12 @@ class MapController < ApplicationController
   end
 
   #登録
-  def new
-    if request.post? then
-      map_data = params[:map_data]
-      map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
-    end
-  end
+  # def new
+  #   if request.post? then
+  #     map_data = params[:map_data]
+  #     map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
+  #   end
+  # end
 
   #編集
   def edit
