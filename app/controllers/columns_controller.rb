a537ddbf15e886e76f5cdc1409c2746e3c9f2034
$ gem install rubocop <s4rthor@gmail.com>
2018-01-21 19:29:37 +0900

    aaaaaa

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -24,6 +24,6 @@ class ColumnsController < ApplicationController
   def destroy
     Column.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to maps_url
+    redirect_to map_url
   end
 end
