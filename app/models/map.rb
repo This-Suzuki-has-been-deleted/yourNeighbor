e95770f92ac056746debd793fe5d092aab302b70
unknown <s20153097@stdlan.hcs.local>
2018-01-22 16:38:10 +0900

    search

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,11 @@
 class Map < ApplicationRecord
   self.primary_keys = :maps_id
   has_many :revews, foreign_key: "maps_id"
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Map.where("map_name like '%" + word + "%'")
+    else
+      Map.all #全て表示。
+    end
+  end
 end
