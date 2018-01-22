0176304f839c6c640c42f2554a1fc939eacc4667
unknown <s20153044@stdlan.hcs.local>
2018-01-22 12:16:19 +0900

    ためしに

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -24,6 +24,6 @@ class ColumnsController < ApplicationController
   def destroy
     Column.find(params[:id]).destroy
     flash[:success] = "User deleted"
-    redirect_to action: 'index', status: :ok
+    redirect_to columns_path
   end
 end
