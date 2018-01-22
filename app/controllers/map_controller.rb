da7643c7f4b2634a33e23829d78ce391714d61b4
unknown <s20153097@stdlan.hcs.local>
2018-01-22 14:55:06 +0900

    map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
 
   #全ピンの情報を取得
   def index
-    @maps = Map.all
+    @map = Map.all
 
   end
 
@@ -10,7 +10,7 @@ class MapController < ApplicationController
   def search
     if request.post? then
       word = params[:word]
-      @maps = map.where("map_name like '%" + word + "%'")
+      @map = map.where("map_name like '%" + word + "%'")
     end
   end
 
@@ -18,15 +18,23 @@ class MapController < ApplicationController
   def new
     if request.post? then
       map_data = params[:map_data]
-      map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_text: map_data[2], map_lat: map_data[3], map_lng: map_data[4], map_date: Date.today.to_time)
+      map.create(maps_id: map.count.to_i + 1, map_name: map_data[1], map_lat: map_data[2], map_lng: map_data[3], map_date: Date.today.to_time)
+      if check.save
+        redirect_to columns_path, notice: '登録しました。'
+      else
+        redirect_to columns_path, notice: '登録に失敗しました。'
     end
   end
 
   #編集
   def edit
     if request.post? then
-      map_id = params[:map_id]
+      map_id = params[:maps_id]
       map.update(maps_id: map_id)
+      if check.save
+        redirect_to columns_path, notice: '更新しました。'
+      else
+        redirect_to columns_path, notice: '更新に失敗しました。'
     end
   end
 
@@ -35,6 +43,9 @@ class MapController < ApplicationController
     if request.post? then
       map_id = params[:map_id]
       map.destroy_all(maps_id: map_id)
+      flash[:success] = "Marker deleted"
+      redirect_to maps_path
+
     end
   end
 
