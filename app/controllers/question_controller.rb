976c590648e892d957e375dc02e3a95c15dd7601
usk <hotmail08ts@gmail.com>
2018-01-23 15:28:42 +0900

    宣言名　変更

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,90 +1,90 @@
-class BoardsController < ApplicationController
-  before_action :set_board, only: [:show, :edit, :update, :destroy]
+class QuestionController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  # GET /boards
-  # GET /boards.json
+  # GET /questions
+  # GET /questions.json
   def index
-    @boards = Board.all
-    @newBoard = Board.new
+    @questions = Question.all
+    @newQuestion = Question.new
   end
 
-  # GET /boards/1
-  # GET /boards/1.json
+  # GET /questions/1
+  # GET /questions/1.json
   def show
-    @board = Board.find(params[:id])
-    @newQuestion = Question.new(:board_id => params[:id])
-    @questions = Question.where(board_id: params[:id])
+    @question = Question.find(params[:id])
+    @newQuestion = Question.new(:question_id => params[:id])
+    @questions = Question.where(question_id: params[:id])
   end
 
-  # GET /boards/new
+  # GET /questions/new
   def new
-    @board = Board.new
-    @board.save
+    @question = Question.new
+    @question.save
   end
 
-  # GET /boards/1/edit
+  # GET /questions/1/edit
   def edit
   end
 
-  # question /boards
-  # question /boards.json
+  # question /questions
+  # question /questions.json
   def create
-    @board = Board.new(params[:board].permit(:title))
-    @board.save
-    redirect_to boards_index_path
-    # @board = Board.new(board_params)
+    @question = Question.new(params[:question].permit(:title))
+    @question.save
+    redirect_to questions_index_path
+    # @question = Question.new(question_params)
     #
     # respond_to do |format|
-    #   if @board.save
-    #     format.html { redirect_to @board, notice: 'Board was successfully created.' }
-    #     format.json { render :show, status: :created, location: @board }
+    #   if @question.save
+    #     format.html { redirect_to @question, notice: 'Question was successfully created.' }
+    #     format.json { render :show, status: :created, location: @question }
     #   else
     #     format.html { render :new }
-    #     format.json { render json: @board.errors, status: :unprocessable_entity }
+    #     format.json { render json: @question.errors, status: :unprocessable_entity }
     #   end
     # end
   end
 
   def delete
-    @board = Board.find(params[:id])
-    @board.destroy
-    redirect_to boards_index_path
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_index_path
   end
 
 
 
-  # PATCH/PUT /boards/1
-  # PATCH/PUT /boards/1.json
+  # PATCH/PUT /questions/1
+  # PATCH/PUT /questions/1.json
   def update
     respond_to do |format|
-      if @board.update(board_params)
-        format.html { redirect_to @board, notice: 'Board was successfully updated.' }
-        format.json { render :show, status: :ok, location: @board }
+      if @question.update(question_params)
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { render :show, status: :ok, location: @question }
       else
         format.html { render :edit }
-        format.json { render json: @board.errors, status: :unprocessable_entity }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /boards/1
-  # DELETE /boards/1.json
+  # DELETE /questions/1
+  # DELETE /questions/1.json
   def destroy
-    @board.destroy
+    @question.destroy
     respond_to do |format|
-      format.html { redirect_to boards_url, notice: 'Board was successfully destroyed.' }
+      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_board
-      @board = Board.find(params[:id])
+    def set_question
+      @question = Question.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def board_params
-      params.require(:board).permit(:title, :content)
+    def question_params
+      params.require(:question).permit(:title, :content)
     end
 end
