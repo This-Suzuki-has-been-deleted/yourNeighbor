c00ee91e317b22cacf0b3f14f17fbd8444575326
usk <hotmail08ts@gmail.com>
2018-01-23 16:07:39 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
+resources :questions
+resources :answers
 #question
   get 'question/index'
   get 'question/show/:id' => 'question#show', as: :question_show
