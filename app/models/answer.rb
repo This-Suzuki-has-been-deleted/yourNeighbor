4dfc39f90f5c6b801f90f5d11f2d13b0a8f60996
unknown <s20153044@stdlan.hcs.local>
2018-01-23 13:49:25 +0900

    1234567890-^\

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,3 @@
 class Answer < ApplicationRecord
+  belongs_to :users, primary_key: "email", foreign_key: "email"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   self.primary_key = "email"
   has_many :maps, foreign_key: "email"
   has_many :reviews, foreign_key: "email"
+  has_many :questions, foreign_key: "email"
+  has_many :answers, foreign_key: "email"
+
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
