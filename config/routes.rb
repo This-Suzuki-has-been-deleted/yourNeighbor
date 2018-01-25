3fff07a13c28039360e8b9f123f5e048b8d8cc81
usk <hotmail08ts@gmail.com>
2018-01-25 10:29:35 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
 }
 resources :questions
 # #question
-  get 'question/index'
-  get 'question/show/:id' => 'question#show', as: :question_show
-  post 'question/create' => 'question#create'
-  delete 'question/delete/:id' => 'question#delete', as: :question_delete
-  get 'question/new' => 'question#new'
+  get 'questions/index'
+  get 'questions/show/:id' => 'questions#show', as: :questions_show
+  post 'questions/create' => 'questions#create'
+  delete 'questions/delete/:id' => 'question#delete', as: :question_delete
+  get 'questions/new' => 'questions#new'
 #answer
   post 'answer/create' => 'answer#create', as: :answer_create
   delete 'answer/delete/:id' => 'answer#delete', as: :answer_delete
