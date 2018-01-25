0b9e1f102db16e7562b326eb0cc77951763beb34
root <root@ip-172-31-28-35.ap-northeast-1.compute.internal>
2018-01-25 13:36:56 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125030502) do
+ActiveRecord::Schema.define(version: 20180125032506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180125030502) do
     t.string "text", limit: 1000
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "map_reports", force: :cascade do |t|
