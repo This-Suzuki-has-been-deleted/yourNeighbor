2c176db8b6e637a808cb178f19bfe37695e6ec34
usk <hotmail08ts@gmail.com>
2018-01-25 13:35:36 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
 self.primary_keys = :question_id
-belongs_to :user, primary_keys: "email", foreign_key: "email"  
+belongs_to :user, primary_key: "email", foreign_key: "email"  
 
 end
