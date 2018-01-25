ec63d8054f59636438b089873ce51bce25152624
unknown <s20153097@stdlan.hcs.local>
2018-01-25 16:38:49 +0900

    nnnfh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Map.create(:maps_id => '1', :map_name => 'スーパーアークス菊水店', :map_lat => 43.053583, :map_lng => 141.378778, :map_date =>, 2018/01/25)
