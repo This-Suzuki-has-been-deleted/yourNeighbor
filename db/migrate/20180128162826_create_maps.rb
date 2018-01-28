823360e602a399084886edbc77d34646b869f5d0
$ gem install rubocop <s4rthor@gmail.com>
2018-01-29 01:30:40 +0900

    a

--- db/migrate/20180128162826_create_maps.rb
+++ db/migrate/20180128162826_create_maps.rb
@@ -1,7 +1,11 @@
 class CreateMaps < ActiveRecord::Migration[5.1]
   def change
     create_table :maps do |t|
-
+            t.string :map_name                                # 場所名
+            t.string :map_text                                # 場所詳細
+            t.float :map_lat                                 # 緯度
+            t.float :map_lng                                 # 経度
+            t.string :email
       t.timestamps
     end
   end
