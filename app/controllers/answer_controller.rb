ea7d9c3b15718fdf88b7aa4108c66c668b339e3e
usk <hotmail08ts@gmail.com>
2018-01-29 10:07:05 +0900

    a

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,6 +1,6 @@
 class AnswerController < ApplicationController
   def create
-    answer = params.require(:answer).permit(:text,:questions_id).merge(email: current_user.username)
+    answer = params.require(:answer).permit(:text).merge(email: current_user.username,questions_id: current_questions.id)
     check = Answer.create(answer)
     if check.save
       redirect_to questions_path, notice: '登録しました。'
