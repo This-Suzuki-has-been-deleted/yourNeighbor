8e659bbc550fb3e9bb9e3c6dfc3dadc1f947f0a5
usk <hotmail08ts@gmail.com>
2018-01-29 15:38:41 +0900

    a

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,6 @@ class QuestionsController < ApplicationController
 
   # GET /question/1/edit
   def edit
-    @question = Question.find(params[:id])
   end
 
   # question /question
@@ -50,6 +49,7 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /question/1
   # PATCH/PUT /question/1.json
   def update
+    @question = Question.find(params[:id])
     @question.assign_attributes(params[:question])
    if @question.save
      redirect_to questions_path, notice: '更新しました。'
