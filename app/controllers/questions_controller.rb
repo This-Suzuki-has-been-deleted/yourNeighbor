bec647713f9837c2ddd9ead6719b59c3079ccb5a
usk <hotmail08ts@gmail.com>
2018-01-29 13:44:28 +0900

    a

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @newAnswer = Answer.new(:question_id => params[:id])
-    @Answers = Answer.where(:question_id: params[:id])
+    @Answers = Answer.where(params[:id]: question_id)
   end
 
   # GET /question/new
