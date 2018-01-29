9821300f65cbe59707b18d97a730527c3ba543d4
usk <hotmail08ts@gmail.com>
2018-01-29 16:21:39 +0900

    a

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -49,8 +49,8 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /question/1
   # PATCH/PUT /question/1.json
   def update
-    @question = params.require(:question).permit(:title, :text, :tag1, :tag2, :tag3 )
-   if @question.save
+    question = params.require(:question).permit(:title, :text, :tag1, :tag2, :tag3 )
+   if question.save
      redirect_to questions_path, notice: '更新しました。'
    else
      redirect_to questions_path, notice: '更新に失敗しました。'
