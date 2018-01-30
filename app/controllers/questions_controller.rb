8be587de6fc65b8768fcb3367d0127a9c4e39f1a
unknown <s20153044@stdlan.hcs.local>
2018-01-30 14:36:17 +0900

    asdghjk

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,7 @@ class QuestionsController < ApplicationController
 
   # GET /question/1/edit
   def edit
-    @question=Question.find(params[:id])
+    @question=Question.find(params[:ids])
   end
 
   # question /question
@@ -50,11 +50,12 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /question/1
   # PATCH/PUT /question/1.json
   def update
-    @question = Question.find(params[:id])
-
-    question = params.require(:question).permit(:title, :text, :tag1, :tag2, :tag3 )
+    @question = find(params[:id])
+    question = params.require(:question).permit(:id, :title, :text, :tag1, :tag2, :tag3 )
     #ここでidを指定して更新　もしくはこの前に指定を済ませて更新する
-      @question.update_attributes(question)
+    if @question.id === question.id
+      Question.update(question)
+    end
       redirect_to questions_path, notice: '更新しました。'
   end
 
