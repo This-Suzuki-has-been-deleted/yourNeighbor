b9405562d9e473361b5ef3ac28a3c0be407424f0
unknown <s20153044@stdlan.hcs.local>
2018-01-30 15:52:48 +0900

    a

--- db/migrate/20180130064805_add_column_to_report.rb
+++ db/migrate/20180130064805_add_column_to_report.rb
@@ -1,5 +1,8 @@
 class AddColumnToReport < ActiveRecord::Migration[5.1]
   def change
     add_column :reports, :report_type, :string
+    add_column :reports, :report_id, :string
+    add_column :reports, :report_email, :string
+    add_column :reports, :report_text, :string
   end
 end
