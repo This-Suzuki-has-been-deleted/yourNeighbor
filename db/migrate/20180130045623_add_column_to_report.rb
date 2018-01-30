183daa65035c12670df3318e5135a2f183bade1d
unknown <s20153044@stdlan.hcs.local>
2018-01-30 14:03:43 +0900

    れぽーと用

--- db/migrate/20180130045623_add_column_to_report.rb
+++ db/migrate/20180130045623_add_column_to_report.rb
@@ -1,5 +1,7 @@
 class AddColumnToReport < ActiveRecord::Migration[5.1]
   def change
     add_column :reports, :report_type, :string
+    add_column :reports, :report_id, :string
+    add_column :reports, :report_email, :string
   end
 end
