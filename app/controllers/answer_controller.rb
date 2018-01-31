43236908ff391d4bb79233faf8a27b4d7a74651d
usk <hotmail08ts@gmail.com>
2018-01-31 14:20:52 +0900

    a

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -3,14 +3,14 @@ class AnswerController < ApplicationController
     answer = params.require(:answer).permit(:text,:question_id).merge(email: current_user.email,username: current_user.username)
     check = Answer.create(answer)
     if check.save
-      redirect_to questions_path, notice: '登録しました。'
+      redirect_to question_path(:question_id), notice: '登録しました。'
     else
       redirect_to questions_path, notice: '登録に失敗しました。'
     end
 
   end
 
-  # GET /question/1/edit
+  # GET /answer/1/edit
   def edit
     @answer=Answer.find(params[:id])
   end
