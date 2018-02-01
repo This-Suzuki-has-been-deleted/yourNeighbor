5518c1887a41455b6fa2d5e921370c398b75c70b
unknown <s20153044@stdlan.hcs.local>
2018-02-01 11:51:50 +0900

    わっかんねぇｗ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
- def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
- end
-
-
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
