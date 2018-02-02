26c02416aab33cb701a4272af6fcd4321577c423
usk <hotmail08ts@gmail.com>
2018-02-02 11:21:41 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
    validates :title, :presence => true
    validates :text, :presence => true
+
    def self.search(tag1) #ここでのself.はUser.を意味する
     if search
       where(['tag1 LIKE ?', "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
