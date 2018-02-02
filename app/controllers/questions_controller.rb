0605b42764ebd38308f9e1cd09dc5154a9de3b56
usk <hotmail08ts@gmail.com>
2018-02-02 11:10:05 +0900

    a

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   # GET /question
   # GET /question.json
   def index
-    @question = Question.where(activated: true).search(params[:search])
+    @question = Question.search(params[:search])
     @newQuestion = Question.new
   end
 
