bddad0e4a058d9e4cd85e81a12665d2ba3672178
usk <hotmail08ts@gmail.com>
2018-02-02 11:13:42 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
    validates :text, :presence => true
    def self.search(tag1) #ここでのself.はUser.を意味する
     if search
-      where(['tag1 LIKE ? || tag2 LIKE ? || tag3 = ?', "%#{tag1}%" , "%#{tag1}%" , "%#{tag1}%"]).select(DISTINCT *) #検索とnameの部分一致を表示。User.は省略
+      where(['tag1 LIKE ? || tag2 LIKE ? || tag3 = ?', "%#{tag1}%" , "%#{tag1}%" , "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
     else
       all #全て表示。User.は省略
     end
