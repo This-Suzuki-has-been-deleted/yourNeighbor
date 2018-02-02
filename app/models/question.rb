e77f20140d71756c84dc966fa828889f4f6d1716
usk <hotmail08ts@gmail.com>
2018-02-02 12:15:59 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,14 +4,14 @@ class Question < ApplicationRecord
 
    validates :title, :presence => true
    validates :text, :presence => true
-
-   def self.search(tag1) #ここでのself.はUser.を意味する
-    if search
-      where(['tag1 LIKE ?', "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
-    else
-      all #全て表示。User.は省略
-    end
-  end
-
-   def persisted? ; false ; end
+  # 
+  #  def self.search(tag1) #ここでのself.はUser.を意味する
+  #   if search
+  #     where(['tag1 LIKE ?', "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
+  #   else
+  #     all #全て表示。User.は省略
+  #   end
+  # end
+  #
+  #  def persisted? ; false ; end
 end
