aa87ad5d299ae1ccdcb9c68ba3a36bc4e4a77e16
usk <hotmail08ts@gmail.com>
2018-02-02 14:34:51 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
       Question.where("(tag1 like ?) OR (tag2 like ?) OR (tag3 like ?)","%#{search}%","%#{search}%","%#{search}%")
     else
-      Qestion.all #全て表示。
+      Question.all #全て表示。
     end
   end
 end
