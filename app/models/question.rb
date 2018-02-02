d247d6f5be9d79961a7503e3c81f2d1d4e0a198c
usk <hotmail08ts@gmail.com>
2018-02-02 12:19:17 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,10 @@
 class Question < ApplicationRecord
-  include ActiveModel::Model
-  include ActiveModel::Conversion
+  # include ActiveModel::Model
+  # include ActiveModel::Conversion
 
    validates :title, :presence => true
    validates :text, :presence => true
-  # 
+  #
   #  def self.search(tag1) #ここでのself.はUser.を意味する
   #   if search
   #     where(['tag1 LIKE ?', "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
