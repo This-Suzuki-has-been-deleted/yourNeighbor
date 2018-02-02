9097fa368d7b76dedffe5c069d10b058ac38769d
usk <hotmail08ts@gmail.com>
2018-02-02 12:21:44 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,17 +1,3 @@
 class Question < ApplicationRecord
-  # include ActiveModel::Model
-  # include ActiveModel::Conversion
 
-   validates :title, :presence => true
-   validates :text, :presence => true
-  #
-  #  def self.search(tag1) #ここでのself.はUser.を意味する
-  #   if search
-  #     where(['tag1 LIKE ?', "%#{tag1}%"]) #検索とnameの部分一致を表示。User.は省略
-  #   else
-  #     all #全て表示。User.は省略
-  #   end
-  # end
-  #
-  #  def persisted? ; false ; end
 end
