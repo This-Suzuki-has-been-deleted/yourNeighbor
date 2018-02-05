eebbf20a8c468a87a06ef1ebc542d3853e226a6e
usk <hotmail08ts@gmail.com>
2018-02-05 09:17:20 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   def self.search(search) #self.でクラスメソッドとしている
-    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+    if search
       Question.where("(tag1 like ?)",search).or("(tag2 like ?)",search).or("(tag3 like ?)" ,search)
     else
       Question.all #全て表示。
