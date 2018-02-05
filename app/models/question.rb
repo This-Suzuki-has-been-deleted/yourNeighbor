0662d9534482b8f8a8f0c5289d4e0671df26ec8f
usk <hotmail08ts@gmail.com>
2018-02-05 09:27:21 +0900

    a

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   def self.search(search) #self.でクラスメソッドとしている
     if search
-      Question.where("(tag1 like ?)",search).or("(tag2 like ?)",search).or("(tag3 like ?)" ,search)
+      Question.where("(tag2 like ?)",search).or("(tag3 like ?)" ,search)
     else
       Question.all #全て表示。
     end
