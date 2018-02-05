9a36c894e7c8c73dfc513dd71f60149e9e07be84
unknown <s20153097@stdlan.hcs.local>
2018-02-05 10:28:37 +0900

    m

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,5 @@
 class MapsController < ApplicationController
-  before_action :set_map, only: %i[show edit update destroy]
+  before_action :set_map, only: %i[show edit update destroy auth_user]
 
   # 全ピンの情報を取得
   def index
